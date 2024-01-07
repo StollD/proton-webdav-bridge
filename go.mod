209b56668ba51c7d56328a12d2ce7abefd9342f7
Dorian Stoll <dorian.stoll@tmsp.io>
2024-01-07 20:55:39 +0100

    Implement login and connecting to Proton Drive

--- go.mod
+++ go.mod
@@ -1,3 +1,44 @@
 module github.com/StollD/proton-webdav-bridge
 
 go 1.21.5
+
+require (
+	github.com/StollD/proton-drive v0.0.0-20240107193102-f89e248ed0d9
+	github.com/adrg/xdg v0.4.0
+	gitlab.com/david_mbuvi/go_asterisks v0.0.0-20221114073100-4669d8bedcbe
+)
+
+require (
+	github.com/ProtonMail/bcrypt v0.0.0-20211005172633-e235017c1baf // indirect
+	github.com/ProtonMail/gluon v0.17.1-0.20230724134000-308be39be96e // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20230717121422-5aa5874ade95 // indirect
+	github.com/ProtonMail/go-mime v0.0.0-20230322103455-7d82a3887f2f // indirect
+	github.com/ProtonMail/go-srp v0.0.7 // indirect
+	github.com/ProtonMail/gopenpgp/v2 v2.7.2 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/barweiss/go-tuple v1.1.2 // indirect
+	github.com/bradenaw/juniper v0.13.1 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/cronokirby/saferith v0.33.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/emersion/go-message v0.16.0 // indirect
+	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
+	github.com/emersion/go-vcard v0.0.0-20230626131229-38c18b295bbd // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/henrybear327/go-proton-api v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/relvacode/iso8601 v1.3.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/term v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
+
+replace github.com/henrybear327/go-proton-api v1.0.0 => github.com/StollD/go-proton-api v0.0.0-20240107183913-ce184ff46634
---
+++ main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/StollD/proton-drive"
+	"github.com/adrg/xdg"
+	"gitlab.com/david_mbuvi/go_asterisks"
+	"os"
+	"time"
+)
+
+const (
+	TokenFile  = "proton-webdav-bridge/tokens.json"
+	AppVersion = "macos-drive@1.0.0-alpha.1+proton-webdav-bridge"
+)
+
+var (
+	OptLogin = false
+)
+
+func doLogin() error {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Enter the username of your Proton Drive account.")
+	fmt.Print("> ")
+
+	user, err := reader.ReadString('\n')
+	fmt.Println()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Enter the password of your Proton Drive account.")
+	fmt.Print("> ")
+
+	pass, err := go_asterisks.GetUsersPassword("", true, os.Stdin, os.Stdout)
+	fmt.Println()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Enter the mailbox password of your Proton Drive account.")
+	fmt.Println("If you don't have a mailbox password, press enter.")
+	fmt.Print("> ")
+
+	mailbox, err := go_asterisks.GetUsersPassword("", true, os.Stdin, os.Stdout)
+	fmt.Println()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Enter a valid 2FA token for your Proton Drive account.")
+	fmt.Println("If you don't have 2FA setup, press enter.")
+	fmt.Print("> ")
+
+	twoFA, err := reader.ReadString('\n')
+	fmt.Println()
+
+	if err != nil {
+		return err
+	}
+
+	credentials := drive.Credentials{
+		Username:        user,
+		Password:        string(pass),
+		MailboxPassword: string(mailbox),
+		TwoFA:           twoFA,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	app := drive.NewApplication(AppVersion)
+	err = app.LoginWithCredentials(ctx, credentials)
+	if err != nil {
+		return err
+	}
+
+	err = storeTokens(*app.Tokens())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Login successful.")
+	return nil
+}
+
+func doListen() error {
+	tokens, err := loadTokens()
+	if err != nil {
+		fmt.Println("Failed to load tokens!")
+		fmt.Println("Run with --login to fix this!")
+		fmt.Println()
+
+		return err
+	}
+
+	fmt.Println("Connecting to Proton Drive ...")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	app := drive.NewApplication(AppVersion)
+	app.LoginWithTokens(&tokens)
+
+	app.OnTokensUpdated(func(tokens *drive.Tokens) {
+		err := storeTokens(*tokens)
+		if err == nil {
+			return
+		}
+
+		fmt.Println("Error storing tokens:", err)
+	})
+
+	app.OnTokensExpired(func() {
+		fmt.Println("The stored tokens are no longer valid!")
+		fmt.Println("Run with --login to fix this!")
+		fmt.Println()
+
+		os.Exit(1)
+	})
+
+	session := drive.NewSession(app)
+
+	err = session.Init(ctx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Connected!")
+
+	time.Sleep(time.Hour)
+	return nil
+}
+
+func loadTokens() (drive.Tokens, error) {
+	var tokens drive.Tokens
+
+	file, err := xdg.DataFile(TokenFile)
+	if err != nil {
+		return tokens, err
+	}
+
+	enc, err := os.ReadFile(file)
+	if err != nil {
+		return tokens, err
+	}
+
+	err = json.Unmarshal(enc, &tokens)
+	if err != nil {
+		return tokens, err
+	}
+
+	return tokens, nil
+}
+
+func storeTokens(tokens drive.Tokens) error {
+	file, err := xdg.DataFile(TokenFile)
+	if err != nil {
+		return err
+	}
+
+	enc, err := json.Marshal(tokens)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(file, enc, 0600)
+}
+
+func main() {
+	var err error = nil
+
+	flag.BoolVar(&OptLogin, "login", OptLogin, "Run Proton Drive login")
+	flag.Parse()
+
+	if OptLogin {
+		err = doLogin()
+	} else {
+		err = doListen()
+	}
+
+	if err != nil {
+		panic(err)
+	}
+}
